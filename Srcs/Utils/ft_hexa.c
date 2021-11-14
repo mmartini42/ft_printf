1c3f3b31ade55108f3888f160bb7bcee492e6327
mathis <mathismartini1@gmail.com>
2021-11-14 20:21:07 +0100

    Remove char * for return

--- Srcs/Utils/ft_hexa.c
+++ Srcs/Utils/ft_hexa.c
@@ -6,27 +6,27 @@
 /*   By: mathmart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 20:08:34 by mathmart          #+#    #+#             */
-/*   Updated: 2021/11/14 20:18:20 by mathmart         ###   ########.fr       */
+/*   Updated: 2021/11/14 20:20:46 by mathmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../Includes/ft_printf.h"
 
-static void	ft_print_hexa(char *base, char *dest, long nbr)
+static void	ft_print_hexa(char *base, long nbr)
 {
 	if (nbr >= ft_strlen(base))
-		ft_print_hexa(base, dest, (nbr / ft_strlen(base)));
+		ft_print_hexa(base, (nbr / ft_strlen(base)));
 	ft_putchar(base[nbr % ft_strlen(base)]);
 }
 
-void	ft_putnbr_base(long nbr, char *dest, char *base)
+void	ft_putnbr_base(long nbr, char *base)
 {
 	if (nbr < 0)
 	{
 		dest[0] = '-';
 		nbr *= -1;
 	}
-	ft_print_hexa(base, dest, nbr);
+	ft_print_hexa(base,nbr);
 }
 
 void	ft_p_hexa(unsigned long long addr, int size)
