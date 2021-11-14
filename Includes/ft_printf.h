751d3bff56b46a7a62c3ef3dc9b235f63f0abe1e
mathis <mathismartini1@gmail.com>
2021-11-14 17:12:42 +0100

    Add attribute protection

---
+++ Includes/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mathmart <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/13 08:56:57 by mathmart          #+#    #+#             */
+/*   Updated: 2021/11/14 17:11:36 by mathmart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+int		ft_printf(const char *str, ...)__attribute__((format(printf, 1, 2)));
+
+#endif
