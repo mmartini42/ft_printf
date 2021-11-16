8e6967aa3228890074d6f278d2fb06f773d8a52e
mathis <mathismartini1@gmail.com>
2021-11-16 02:01:49 +0100

    Update the path to header file

--- Srcs/Functions/ft_arg_s.c
+++ Srcs/Functions/ft_arg_s.c
@@ -6,11 +6,11 @@
 /*   By: mathmart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 21:08:02 by mathmart          #+#    #+#             */
-/*   Updated: 2021/11/16 01:19:13 by mathmart         ###   ########.fr       */
+/*   Updated: 2021/11/16 01:37:50 by mathmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../Includes/ft_printf.h"
+#include "../../Includes/ft_printf.h"
 
 int	ft_print_s(va_list ap)
 {
--- Srcs/Utils/ft_putstr.c
+++ Srcs/Utils/ft_putstr.c
@@ -6,11 +6,11 @@
 /*   By: mathmart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 21:09:25 by mathmart          #+#    #+#             */
-/*   Updated: 2021/11/16 00:27:37 by mathmart         ###   ########.fr       */
+/*   Updated: 2021/11/16 01:37:38 by mathmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../Includes/ft_printf.h"
+#include "../../Includes/ft_printf.h"
 
 int	ft_putstr(const char *str)
 {
