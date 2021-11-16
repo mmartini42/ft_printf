c385071b5543b7b8593a2dc07391876922497131
mathis <mathismartini1@gmail.com>
2021-11-16 01:26:52 +0100

    Final psuh

--- Includes/ft_printf.h
+++ Includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mathmart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 08:56:57 by mathmart          #+#    #+#             */
-/*   Updated: 2021/11/14 21:40:47 by mathmart         ###   ########.fr       */
+/*   Updated: 2021/11/16 01:16:27 by mathmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,24 +28,22 @@ int		ft_printf(const char *str, ...)__attribute__((format(printf, 1, 2)));
 
 /* FUNCTIONS */
 
-void	ft_print_c(va_list ap);
-void	ft_print_s(va_list ap);
-void	ft_print_p(va_list ap);
-void	ft_print_d(va_list ap);
-void	ft_print_i(va_list ap);
-void	ft_print_u(va_list ap);
-void	ft_print_x(va_list ap);
-void	ft_print_majx(va_list ap);
-void	ft_print_percent(va_list ap);
+int		ft_print_c(va_list ap);
+int		ft_print_s(va_list ap);
+int		ft_print_p(va_list ap);
+int		ft_print_d(va_list ap);
+int		ft_print_i(va_list ap);
+int		ft_print_u(va_list ap);
+int		ft_print_x(va_list ap);
+int		ft_print_majx(va_list ap);
+int		ft_print_percent(va_list ap);
 
 /* UTILS */
 
-void	ft_putnbr(int n);
-void	ft_putunbr(unsigned int nb);
-void	ft_putchar(char c);
-void	ft_putstr(const char *str);
-void	ft_putnbr_base(unsigned int nbr, char *base);
-void	ft_p_hexa(unsigned long long addr, int size);
+void	ft_putnbr(int n, unsigned int *count);
+void	ft_putunbr(unsigned int nb, unsigned int *count);
+int		ft_putchar(char c);
+int		ft_putstr(const char *str);
 size_t	ft_strlen(const char *str);
 
 #endif
--- Makefile
+++ Makefile
@@ -6,44 +6,38 @@
 #    By: mathmart <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/13 08:52:08 by mathmart          #+#    #+#              #
-#    Updated: 2021/11/14 21:23:55 by mathmart         ###   ########.fr        #
+#    Updated: 2021/11/16 01:26:25 by mathmart         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
--include Sources.mk
-
 NAME = libftprintf.a
+
+SRCS = Srcs/main.c Srcs/Functions/ft_arg_c.c Srcs/Functions/ft_arg_d.c \
+	Srcs/Functions/ft_arg_i.c Srcs/Functions/ft_arg_majx.c Srcs/Functions/ft_arg_p.c \
+	Srcs/Functions/ft_arg_percent.c Srcs/Functions/ft_arg_s.c Srcs/Functions/ft_arg_u.c \
+	Srcs/Functions/ft_arg_x.c Srcs/Utils/ft_putchar.c Srcs/Utils/ft_putnbr.c \
+	Srcs/Utils/ft_putstr.c Srcs/Utils/ft_putunbr.c Srcs/Utils/ft_strlen.c
 
 OBJS = $(SRCS:.c=.o)
 
-HEADER = ./Includes
+HEADER = ./Includes/ft_printf.h
 
 CFLAGS = -Wall -Wextra -Werror -O3 -g -I Includes
 
 %.o: %.c $(HEADER)
 	gcc $(CFLAGS) -c $< -o $@
 
-$(NAME): gmk $(OBJS)
+$(NAME): $(OBJS)
 	ar rcs $(NAME) $(OBJS)
 
 all: gmk $(NAME)
-
-test: $(OBJS)
-	gcc $(OBJS) -o test
 
 clean:
 	rm -rf $(OBJS)
 
 fclean: clean
-	rm -rf $(NAME)
+	rm -rf $(NAME) test
 
 re: fclean all
 
-gmk:
-	echo "SRCS +=" > Sources.mk
-	echo "`find Srcs -name '*.c' | sed 's/^/SRCS += /'`" >> Sources.mk
-
-norm:
-	norminette
-
-.PHONY: norm gmk re fclean clean all
+.PHONY: re fclean clean all
